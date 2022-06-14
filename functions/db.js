2b2fa0ce69504c1176b5fbaadf26758dccc9c56c
Roshanac <acharyaresham354@gmail.com>
2022-06-14 22:33:29 +0545

    intial commit

--- functions/db.js
+++ functions/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-// const mongouri="mongodb+srv://Roshanac:kali@cybergeek.vlnmz.mongodb.net/?retryWrites=true&w=majority";
-const mongouri='mongodb://localhost:27017/cybergeek'
+const mongouri="mongodb+srv://Roshanac:kali@cybergeek.vlnmz.mongodb.net/?retryWrites=true&w=majority";
+// const mongouri='mongodb://localhost:27017/cybergeek'
 const MongoConnect=()=>{
     mongoose.connect(mongouri,()=>{
         console.log('mongodb is connected successfully')
